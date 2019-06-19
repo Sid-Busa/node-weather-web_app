25f81e9c702cee4c8cc237aecbd20f146514b137
Sid-Busa <sidbusa81@gmail.com>
2019-06-19 08:56:17 +0530

    Remove Console.log unnessasari line

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,3 @@
-console.log("Client side scripting is running")
-
-// fetch("http://puzzle.mead.io/puzzle").then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
-// fetch("http://localhost:3000/weather?address=!").then((response)=>{
-//     response.json().then((data) => {
-//         if(data.error){
-//             return console.log("Error")
-//         }
-//         else{
-//         console.log(data.forecast)
-//         console.log(data.location)
-//         console.log(data.address)
-//         }  
-//     })
-// })
-
-
 
 const weatherForm = document.querySelector("form")
 const search = document.querySelector("input")
